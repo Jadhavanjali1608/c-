2713b7fbace160b134c4225e97012a3eee7eebdc
Anjali Jadhav <jadhavanjali@gmail.com>
2026-04-11 22:43:40 +0530

    Celsius to Fahrenheit

---
+++ code21.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b, gcd = 1, lcm;
+
+        Console.Write("Enter first number: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        // Find GCD
+        for (int i = 1; i <= a && i <= b; i++)
+        {
+            if (a % i == 0 && b % i == 0)
+                gcd = i;
+        }
+
+        // Find LCM
+        lcm = (a * b) / gcd;
+
+        Console.WriteLine("GCD = " + gcd);
+        Console.WriteLine("LCM = " + lcm);
+    }
+}

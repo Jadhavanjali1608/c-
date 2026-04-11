cf736689eead220d337394365a9981054894c4f3
Anjali Jadhav <jadhavanjali@gmail.com>
2026-04-11 22:35:18 +0530

    Multiplication nTable

---
+++ code18.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num;
+
+        Console.Write("Enter a number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Multiplication Table:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(num + " x " + i + " = " + (num * i));
+        }
+    }
+}

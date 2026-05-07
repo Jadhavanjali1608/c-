7455fb3e304f501acc1398a0519866eedf77a6bc
Anjali Jadhav <jadhavanjali@gmail.com>
2026-05-07 10:35:44 +0530

    role-based

---
+++ Level2/Level3/Level4/cose170.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// JWT Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey =
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("MySecretKey123"))
+    };
+});
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+// Controller
+[ApiController]
+[Route("api/[controller]")]
+public class LoginController : ControllerBase
+{
+    // Generate Token
+    [HttpGet]
+    public IActionResult Login()
+    {
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes("MySecretKey123"));
+
+        var creds = new SigningCredentials(
+            key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            claims: new[]
+            {
+                new Claim(ClaimTypes.Name, "Anjali"),
+                new Claim(ClaimTypes.Role, "Admin")
+            },
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: creds
+        );
+
+        string jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return Ok(jwt);
+    }
+
+    // Only Admin Access
+    [Authorize(Roles = "Admin")]
+    [HttpGet("admin")]
+    public IActionResult AdminData()
+    {
+        return Ok("Welcome Admin");
+    }
+}
